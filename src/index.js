e921bf0f3729fcabd23913921d11edd43c0a2159
Teufelex <gloomycoke@gmail.com>
2019-03-15 13:08:12 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  var i;
+  var f = 0;
+  for (i = 0; i < preferences.length; i++) {
+    var n = preferences[i];
+    var k = preferences[n-1];
+    var m = preferences[k-1];
+    if (m == i+1 && k!=n && k!=m && m!=n) {
+      f++;
+    }
+    }
+  f=f/3;
+return f;
+}
+  
